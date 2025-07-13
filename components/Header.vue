058dc4d07b7bd887f01d431784276ce54a9a1574
DonValen910 <valentinstito@gmail.com>
2025-07-13 20:50:39 -0300

    feat: agregar componente Header con navegación y logo

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="w-full h-[175px] py-15px px-70px flex items-center justify-between">
+    <figure class="w-200px">
+      <img src="../public/logo_transparente.png" alt="Logo de Liga Manager">
+    </figure>
+    <nav>
+      <ul class="flex gap-40px">
+        <li><a class="text-lg" href="#">Equipos</a></li>
+        <li><a class="text-lg" href="#">Jugadores</a></li>
+        <li><a class="text-lg" href="#">Fixtures</a></li>
+        <li><a class="text-lg" href="#">Resultados</a></li>
+        <li><a class="text-lg" href="#">Estadisticas</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
