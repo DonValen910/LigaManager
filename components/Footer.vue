10224604f8eee8b3f4abb1168d739875f08e0e8c
DonValen910 <valentinstito@gmail.com>
2025-07-18 14:09:46 -0300

    feat: agregar componente Footer y actualizar app.vue para incluirlo

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="flex justify-between py-15px px-70px">
+    <figure class="w-200px">
+      <img src="../public/logo_transparente.png" alt="Logo de Liga Manager">
+    </figure>
+
+    <div class="grid content-between">
+      <div>
+        <span class="text-lg block text-center font-bold">Enlaces utiles</span>
+        <ul class="flex gap-40px ">
+          <li><a class="text-lg" href="#">Acerca de</a></li>
+          <li><a class="text-lg" href="#">Contacto</a></li>
+          <li><a class="text-lg" href="#">Términos de uso</a></li>
+          <li><a class="text-lg" href="#">Política de privacidad</a></li>
+        </ul>
+      </div>
+
+      <span class="flex justify-center items-center">Hecho con ❤️ y Vue.js | © 2025 Liga Manager | <figure class="ml-5px"><img
+            src="../public/github-mark.svg" alt="Icono de GitHub"></figure></span>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
