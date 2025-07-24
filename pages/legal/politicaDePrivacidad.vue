128762045c4637e1c91d6943c651db967150566e
DonValen910 <valentinstito@gmail.com>
2025-07-24 01:30:54 -0300

    feat: agregar páginas legales: Acerca de, Contacto, Política de Privacidad y Términos de Uso

---
+++ pages/legal/politicaDePrivacidad.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Política de Privacidad Page</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/legal/terminoDeUso.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Términos de Uso Page</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
